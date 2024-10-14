382c5c909539ab52a5cc5a5bf6d2dffdb2bdb048
marovacka <honek@cngroup.dk>
2024-10-14 12:02:17 +0200

    - added inference

--- {{cookiecutter.project_dir}}/src/{{cookiecutter.project_dir}}/helpers/helpers.py
+++ {{cookiecutter.project_dir}}/src/{{cookiecutter.project_dir}}/helpers/helpers.py
@@ -1,3 +1,14 @@
+import pandas as pd
+
 def hello_{{cookiecutter.project_dir}}():
     print(f"hello {{cookiecutter.project_dir}}")
+
+def get_wine_dataset():
+    white_df = pd.read_csv("../data/winequality-white.csv", delimiter=';')
+    white_df['is_red'] = 0
+    red_df =  pd.read_csv("../data/winequality-red.csv", delimiter=';')
+    red_df['is_red'] = 1
+    wine_dataset_df = pd.concat((white_df, red_df))    
+
+    return wine_dataset_df
     
